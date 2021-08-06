d33241e1912f03c5e24df3a398917bebde76d316
celeis <gonca.negis@gmail.com>
2021-08-06 16:27:06 +0300

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App'
+
+describe('App testleri', () => {
+  let inputElement, headerElement, resultsElement, hundredElement
+
+  beforeEach(() => {
+    render(<App />)
+    inputElement = screen.getByAltText('input')
+    headerElement = screen.getByText(/emoji search/i)
+    resultsElement = screen.getByTestId('results-list')
+    hundredElement = screen.getByAltText('100')
+  })
+
+  test('renders header', () => {
+    expect(headerElement).toBeInTheDocument()
+  })
+
+  test('renders emoji list', () => {
+    expect(resultsElement).toBeInTheDocument()
+  })
+
+  test('allows users to type into input field', () => {
+    // Simulate user events
+    fireEvent.change(inputElement, { target: { value: 'Smile' } })
+
+    // Make assertion
+    expect(inputElement.value).toBe('Smile')
+  })
+
+  test('renders correct results on search', () => {
+    // Simulate user events
+    fireEvent.change(inputElement, { target: { value: 'Smile' } })
+
+    // Make assertion
+    expect(resultsElement).toHaveTextContent('Smile')
+  })
+
+  test('copies to clipboard when clicked', () => {
+    // Simulate user events
+    userEvent.click(hundredElement)
+    userEvent.paste(inputElement, '100')
+    // Make assertion
+    expect(inputElement).toHaveValue('100')
+  })
+})
